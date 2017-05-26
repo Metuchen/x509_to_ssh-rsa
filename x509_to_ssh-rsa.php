20f394a032e8ca32e62d5f02ce6eeca706b0f3bf
Mark Tomich <mtomich@netsilo.metuchen.dyndns.org>
2017-05-26 14:16:46 -0400

    minor language updates

--- x509_to_ssh-rsa.php
+++ x509_to_ssh-rsa.php
@@ -12,10 +12,10 @@ Fork this code on <a href='https://github.com/Metuchen/x509_to_ssh-rsa'>GitHub</
     }
     return hex2bin(sprintf($format, $len, bin2hex($data)));
   }
-  if (empty($_SERVER['SSL_CLIENT_CERT'])) print ("You must present a client certificate for it to be useful to you.\n");
+  if (empty($_SERVER['SSL_CLIENT_CERT'])) print ("You must present a client certificate for this site to be useful to you.\n");
   else {
     $pub_key=openssl_pkey_get_details(openssl_pkey_get_public($_SERVER['SSL_CLIENT_CERT']));
-    if (! isset($pub_key['rsa'])) print ("Oops...I can't find an RSA key.  Feel free to fork us on GitHub if you feel like implementing something else (it shouldn't be too hard.)\n");
+    if (! isset($pub_key['rsa'])) print ("Oops...I can't find an RSA key.  Feel free to fork us on GitHub if you feel like implementing something else.\n");
     else {
       printf('<p style="font-family:monospace">ssh-rsa %s</p>', base64_encode(mpint('ssh-rsa').mpint($pub_key['rsa']['e']).mpint($pub_key['rsa']['n'])));
     }
